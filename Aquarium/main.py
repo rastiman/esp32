e8745550835fb1f315f7b11bd9f3d354c20d5dc8
Rasto17 <Rasto17@users.noreply.github.com>
2018-01-25 16:32:43 +0100

    -

--- Aquarium/main.py
+++ Aquarium/main.py
@@ -1,5 +1,7 @@
 import machine
 from umqtt.robust import MQTTClient
+import onewire 
+import ds18x20 
 
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -7,14 +9,35 @@ with open('config.json') as config_file:
 mqtt = MQTTClient(config["device_name"], config["mqtt_server"],
                   user=config["mqtt_user"], password=config["mqtt_pass"])
 
+
+
 rel1 = machine.Pin(12, machine.Pin.OUT)
 rel2 = machine.Pin(14, machine.Pin.OUT)
 rel3 = machine.Pin(27, machine.Pin.OUT)
 
+ow = onewire.OneWire(machine.Pin(19))
+
+#rtc = machine.RTC()
+
+sensors = ds18x20.DS18X20(ow)
+
+# scan for devices on the bus
+roms = sensors.scan()
+print('Found devices:', roms)
+
 def set_device():
     with open('setting.json') as setting_file:
         setting = json.load(setting_file)
-        
 
+def measure():
+    """Perform DS18B20 measurement and return json message to publish."""
+    sensors.convert_temp()
+    sleep_ms(750)
+    temperature = [sensors.read_temp(t) for t in sensors.scan()]
+    try:
+        print("Temp: " + str(temperature[0]) + chr(176) + "C")
+        return json.dumps({'temp': temperature[0]})
+    except IndexError:
+        print("Sensor Error!")
 
-print ("do nothing")
+measure()
